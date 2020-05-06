19257ddb0e5e796085f15d4fbfbcf9052317686f
gauravkhurana97 <gauravkhurana97>
2020-05-07 03:35:08 +0530

    Undo_Redo Sync Functionality

--- express.js
+++ express.js
@@ -14,20 +14,36 @@ app.use(express.static("public"));
 // server
 
 io.on("connection", function (socket) {
-  socket.on("size", function (size) {
-    socket.broadcast.emit("onsize", size);
-  });
-  socket.on("color", function (color) {
-    socket.broadcast.emit("oncolor", color);
-  });
+    socket.on("size", function (size) {
+        socket.broadcast.emit("onsize", size);
+    });
+    socket.on("color", function (color) {
+        socket.broadcast.emit("oncolor", color);
+    });
 
-  socket.on("toolchange", function (tool) {
-    socket.broadcast.emit("ontoolchange", tool);
-  });
+    socket.on("toolchange", function (tool) {
+        socket.broadcast.emit("ontoolchange", tool);
+    });
+
+    socket.on("hamburger", function () {
+        socket.broadcast.emit("onhamburger");
+    });
+    socket.on("mousedown", function (point) {
+        socket.broadcast.emit("onmousedown", point);
+    });
+    socket.on("mousemove", function (point) {
+        socket.broadcast.emit("onmousemove", point);
+    });
+    socket.on("undo", function () {
+        socket.broadcast.emit("onundo");
+    });
+    socket.on("redo", function () {
+        socket.broadcast.emit("onredo");
+    });
 });
 
 // nodejs server
 const port = process.env.PORT || 3000;
 server.listen(port, function (req, res) {
-  console.log("Server has started at port 3000");
+    console.log("Server has started at port 3000");
 });
--- public/js/receiver.js
+++ public/js/receiver.js
@@ -7,3 +7,34 @@ socket.on("onsize", function(size) {
   socket.on("ontoolchange", function(tool) {
     handleToolChange(tool);
   });
+
+  socket.on("onmousedown", function(point) {
+    const { x, y, color, width } = point;
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    undoStack.push(point);
+  });
+  socket.on("onmousemove", function(point) {
+    const { x, y, color, width } = point;
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    undoStack.push(point);
+  });
+  socket.on("onundo", function() {
+    undoMaker();
+  });
+  socket.on("onredo", function() {
+    redoMaker();
+  });
+
+  
+socket.on("onhamburger", function() {
+    handleHamburger();
+  });
+  
+
+  
